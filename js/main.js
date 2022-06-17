952f67de6fe7ce8ac7dba1849c3997b75de28af5
Vanessa Mendez <irsmp12@gmail.com>
2022-06-17 07:36:38 -0500

    Give styles to fonts, borders and button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const email = document.getElementById('email')
 const button = document.getElementById('button')
 const errorMsg = document.getElementById('error-msg')
+const svg = document.getElementById('svg')
 
 function verifyEmail(email){
   const regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
   let checkOut = regExp.test(email.value)
   if (!checkOut) {
     errorMsg.classList.add('error-visible')
+    email.style.outlineColor = 'hsl(354, 100%, 66%)'
     email.value = ''
     email.focus()
   } else {
